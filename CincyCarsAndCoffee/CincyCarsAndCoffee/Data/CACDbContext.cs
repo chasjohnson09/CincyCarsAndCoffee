795bd538a6e0290008ad36c7419977c787c08bec
Chas Johnson <chasjohnson09@gmail.com>
2021-05-04 10:53:06 -0400

    First day Commit

---
+++ CincyCarsAndCoffee/CincyCarsAndCoffee/Data/CACDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CincyCarsAndCoffee.Data
+{
+    class CACDbContext : DbContext
+    {
+        public CACDbContext (DbContextOptions<CACDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CincyCarsAndCoffee.Models.North> North { get; set; }
+        public DbSet<CincyCarsAndCoffee.Models.North> East { get; set; }
+        public DbSet<CincyCarsAndCoffee.Models.North> South { get; set; }
+        public DbSet<CincyCarsAndCoffee.Models.North> West { get; set; }
+
+    }
+}
