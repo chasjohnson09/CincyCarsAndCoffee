5d18dc8569da1bc191bbddee20d9ba3c7fa91cee
Chas Johnson <chasjohnson09@gmail.com>
2021-06-08 15:52:35 -0400

    new backend

---
+++ Cincy_Cars/Cincy_Cars/CCACDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cincy_Cars
+{
+    public class CCACDbContext : DbContext
+    {
+        public CCACDbContext(DbContextOptions<CCACDbContext> options)
+               : base(options)
+        {
+        }
+
+        public DbSet<Cincy_Cars.Models.Downtown> Dowtown { get; set; }
+        public DbSet<Cincy_Cars.Models.North> North { get; set; }
+        public DbSet<Cincy_Cars.Models.South> South { get; set; }
+        public DbSet<Cincy_Cars.Models.East> East { get; set; }
+        public DbSet<Cincy_Cars.Models.West> West { get; set; }
+    }
+}
---
+++ Cincy_Cars/Cincy_Cars/Models/East.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cincy_Cars.Models
+{
+    public class East
+    {
+        public int Id { get; set; }
+        [StringLength(200), Required]
+        public string Name { get; set; }
+        [StringLength(200), Required]
+        public string Organization { get; set; }
+        [Required]
+        public DateTime DateTime { get; set; }
+        [StringLength(500)]
+        public string Link { get; set; }
+    }
+}
