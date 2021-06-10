0ceda68c4bc296150f7e53c6ac39414b2bc9a31c
Chas Johnson <chasjohnson09@gmail.com>
2021-06-10 15:26:20 -0400

    entity framework adding

--- Cincy_Cars/Cincy_Cars/Startup.cs
+++ Cincy_Cars/Cincy_Cars/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,11 @@ namespace Cincy_Cars
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<CCACDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("CCACDb")));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
